4b36f7876499f51c12af20a07a4e68c323afd917
Yaroslav Molochko <y.molochko@anchorfree.com>
2017-10-24 14:34:22 +0300

    Add quota plugin

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,15 @@ RUN apk add --update --virtual .build-deps \
         sudo build-base ruby-dev \
  && sudo gem install \
         fluent-plugin-elasticsearch \
- && sudo gem install \
         fluent-plugin-record-reformer \
- && sudo gem install \
         fluent-plugin-rewrite-tag-filter \
- && sudo gem install \
         fluent-plugin-multiprocess \
- && sudo gem install \
         fluent-plugin-multi-format-parser \
+        redis \
+        gcra \
  && sudo gem sources --clear-all \
  && apk del .build-deps \
  && rm -rf /var/cache/apk/* \
            /home/fluent/.gem/ruby/2.3.0/cache/*.gem
+
+COPY ./plugins/* /fluentd/plugins/
---
+++ plugins/filter_quota.rb
@@ -0,0 +1,64 @@
+require 'fluent/plugin/filter'
+require 'redis'
+require 'gcra/rate_limiter'
+require 'gcra/redis_store'
+
+module Fluent::Plugin
+  class ThrottleFilter < Filter
+    # Register this filter as "quota"
+    Fluent::Plugin.register_filter('quota', self)
+
+    config_param :rate_period, :float, default: 1.0
+    config_param :max_burst, :integer, default: 1
+    config_param :redis_url, :string, default: ""
+    config_param :prefix, :string, default: "default"
+
+    attr_accessor :redis
+
+    def configure(conf)
+      super
+      # do the usual configuration here
+    end
+
+    def start
+      super
+      # This is the first method to be called when it starts running
+      # Use it to allocate resources, etc.
+      @counter = 0
+      @remaining = 0
+      @redis = Redis.new(url: @redis_url)
+      Thread.new {
+      @store = GCRA::RedisStore.new(@redis, @prefix)
+
+      @limiter = GCRA::RateLimiter.new(@store, @rate_period, @max_burst)
+      }
+    end
+
+    def shutdown
+      super
+      @redis.close()
+      # This method is called when Fluentd is shutting down.
+      # Use it to free up resources, etc.
+    end
+
+    def filter(tag, time, record)
+      # This method implements the filtering logic for individual filters
+      # It is internal to this class and called by filter_stream unless
+      # the user overrides filter_stream.
+      #
+      # If returns nil, that records are ignored.
+
+      if @counter < @remaining
+          @counter += 1
+          return record
+      end
+      exceeded, info = @limiter.limit(@prefix, @counter)
+      @remaining = info.remaining
+      @counter = 0
+      if exceeded
+          return nil
+      end
+      record
+    end
+  end
+end
